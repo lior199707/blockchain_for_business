7d761b5857702ba6eb978f50aa8dd8312a1e7eb4
Lior <Shilon199707@gmail.com>
2022-07-20 09:38:55 +0300

    class User handles users in the server. a user has: * address -> dict, keys: ip, port * writer -> asyncio.StreamWriter * reader -> asyncio.StreamReader * amount -> money a user can: * Send messages * receive messages

---
+++ funcoin_business/user.py
@@ -0,0 +1,68 @@
+import asyncio
+
+
+class User:
+
+    # class Address
+    def __init__(self,
+                 writer: asyncio.StreamWriter,
+                 reader: asyncio.StreamReader,
+                 amount: float,
+                 miner: bool,
+                 address: dict,
+                 ):
+        self.writer = writer
+        self.reader = reader
+        self.amount = amount
+        self.is_miner = miner
+        self.address = address
+
+    def get_writer(self) -> asyncio.StreamWriter:
+        """
+
+        :return:asyncio.StreamWriter, the writer of the user
+        """
+        return self.writer
+
+    def get_reader(self) -> asyncio.StreamReader:
+        """
+
+        :return: asyncio.StreamReader, the reader of the user
+        """
+        return self.reader
+
+    def is_miner(self) -> bool:
+        """
+
+        :return:boolean, true if the user is a miner, false otherwise
+        """
+
+        return self.is_miner
+
+    def get_ip(self) -> str:
+        """
+
+        :return:str, the ip of the user
+        """
+        return self.address["ip"]
+
+    def get_port(self) -> int:
+        """
+
+        :return:int, the port of the user
+        """
+        return self.address["port"]
+
+    def get_address(self) -> str:
+        """
+
+        :return:String, ip:port of the user
+        """
+        return f'{self.get_ip()}:{self.get_port()}'
+
+    async def receive_message(self, message: str):
+        self.writer.write(f'{message}\r\n'.encode())
+
+    async def respond(self):
+        response = await self.reader.readuntil(b"\n")
+        return response.decode("utf8").strip()
