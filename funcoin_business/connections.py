b1a4352dc6c10fa43ab5e4e0c628371799dd15f9
Lior <Shilon199707@gmail.com>
2022-07-20 09:47:36 +0300

    class ConnectionPool, handles connected and authorized users on the server. contains a dict , key -> "IP:port", value -> user object. can add users to the pool, remove them, and return a list containing a specified number of users.

---
+++ funcoin_business/connections.py
@@ -0,0 +1,55 @@
+import structlog
+from more_itertools import take
+
+import funcoin_business.user
+
+logger = structlog.getLogger(__name__)
+
+
+class ConnectionPool:
+    """
+    Class ConnectionPool handles all the users connected to the server
+    has a dictionary with:
+    keys - user addresses (ip:port)
+    value - the user, funcoin_business.user.User
+    """
+    def __init__(self):
+        self.connection_pool = dict()
+
+    async def broadcast(self, message: str):
+        """
+        sends a message to all the user connected to the server
+        :param message:  the message to send
+        """
+        for user in list(self.connection_pool.values()):
+            await user.receive_message(message)
+
+    def add_peer(self, user: funcoin_business.user.User):
+        """adds a user to the dictionary of the connected users"""
+        address = user.get_address()
+        self.connection_pool[address] = user
+        logger.info("Added new peer to pool", address=address)
+
+    def remove_peer(self, user: funcoin_business.user.User):
+        """Removes a user from the dictionary of the connected users"""
+        address = user.get_address()
+        self.connection_pool.pop(address)
+        logger.info("Removed peer from pool", address=address)
+
+    def get_alive_peers(self, count):
+        """
+
+        :param count: the number of wanted users.
+        :return: list containing the first 'count' users in the pool
+        """
+        # TODO (Reader): Sort these by most active,
+        #  but let's just get the first *count* of them for now
+        return take(count, self.connection_pool.items())
+        # return [user.address for user in list(self.connection_pool.values())[:count]]
+
+    def get_size(self) -> int:
+        """
+
+        :return: The number of authorized users connected to the server
+        """
+        return len(self.connection_pool)
