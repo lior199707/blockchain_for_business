f791fd3b80d237d40edb9512fe2e73515a093070
Lior <Shilon199707@gmail.com>
2022-08-10 19:46:54 +0300

    (Ref) added import to P2PProtocol and Controller

--- node.py
+++ node.py
@@ -3,6 +3,8 @@ import asyncio
 from funcoin_business.blockchain import Blockchain
 from funcoin_business.server import Server
 from funcoin_business.connections import ConnectionPool
+from funcoin_business.peers import P2PProtocol
+from funcoin_business.controller.controller import Controller
 
 # Instantiate the blockchain and our pool for "peers"
 blockchain = Blockchain()
