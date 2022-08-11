b6db11fa4291dff464093053ccb953e82a535d35
Lior <Shilon199707@gmail.com>
2022-08-11 15:16:20 +0300

    (Add) imports: nacl.signing.VerifyKey -> used in order to verify a transaction by the creator's signature nacl.encoding.HexEncoder -> the encoder to verify the transaction with. nacl.exceptions.BadSignatureError -> used to detect fraudulent transactions. import json -> used to convert data to bytes

--- funcoin_business/transactions/transactions.py
+++ funcoin_business/transactions/transactions.py
@@ -1,9 +1,13 @@
-
+import json
 from time import time
+
+from nacl.exceptions import BadSignatureError
 
 from funcoin_business.schema import CarSchema
 from funcoin_business.cars.car import Car
 from funcoin_business.users.user import User
+from nacl.signing import VerifyKey
+from nacl.encoding import HexEncoder
 
 
 def create_transaction(sender: User, receiver: User, car: Car) -> dict:
